da6874336dd1142e24120a34de249f624b8f884a
jisoooit <ydsn336@naver.com>
2022-09-16 16:44:03 +0900

    fix: Add output comment
    example of result

--- fibonachicken.py
+++ fibonachicken.py
@@ -2,4 +2,4 @@
 
 
 num = int(input("Enter a natual number: ")) #ex. num : 5
-print("A golden ratio of {} is {}".format(num, get_golden_ratio(num)))
+print("A golden ratio of {} is {}".format(num, get_golden_ratio(num))) #result: 3

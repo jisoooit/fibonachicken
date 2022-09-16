9f969b2c8db766ef8a4381d72785a70234bbe354
jisoooit <ydsn336@naver.com>
2022-09-16 16:41:44 +0900

    fix: Add a comment
    comment - example of num

--- fibonachicken.py
+++ fibonachicken.py
@@ -1,4 +1,5 @@
 #여기에 피보나치킨 코드를 써주세요
 
-num = int(input("Enter a natual number: "))
+
+num = int(input("Enter a natual number: ")) #ex. num : 5
 print("A golden ratio of {} is {}".format(num, get_golden_ratio(num)))
